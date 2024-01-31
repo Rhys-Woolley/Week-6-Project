4f4b4b12a7ff01837e5efb766f224f77cbe944ac
Ash <56586401+AshCaiH@users.noreply.github.com>
2024-01-31 13:48:57 +0000

    Styled Cat component

--- src/components/Cat.jsx
+++ src/components/Cat.jsx
@@ -5,15 +5,32 @@ import '../App.css'
 const Cat = (props) => {
   return (
     <>
-      <CatPic src={props.url}/>
+      <CatPicHolder>
+        <CatPic src={props.url}/>
+      </CatPicHolder>
     </>
   )
 }
 
 export default Cat
 
+const imageHeight = 250;
+const imageWidth = 350;
+
 const CatPic = styled.img`
-  height: 250px;
-  width: 250px;
+  height: ${imageHeight}px;
+  width: ${imageWidth}px;
   object-fit: cover;
+  transition: 0.3s ease-out;
+  z-index: 0;
+
+  &:hover {
+    scale: 1.2;
+  }
+`
+
+const CatPicHolder = styled.div`
+  height: ${imageHeight}px;
+  width: ${imageWidth}px;
+  overflow: hidden;
 `
