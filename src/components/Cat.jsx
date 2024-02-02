30de4c1593374135aa533161737eb9d30b242268
Ash <56586401+AshCaiH@users.noreply.github.com>
2024-02-02 10:50:22 +0000

    Fixed buttons falling out of the cat component (for real this time)

--- src/components/Cat.jsx
+++ src/components/Cat.jsx
@@ -20,7 +20,7 @@ const CatComponent = (props) => {
         <CatNameTag>{props.cat.name}</CatNameTag>
         <CatPrice>£{props.cat.price}</CatPrice>
       </div>
-      <div className="flex right">
+      <div className="flex right end">
         <CatButton onClick={() => props.infoFunc(props.cat)}>< FaInfoCircle /></CatButton> 
 
         {!inBasket ? 
@@ -68,6 +68,7 @@ const CatPicHolder = styled.div`
   border-radius: 5px;
   margin: 10px;
   display: flex;
+  flex-direction: column;
 
   &:hover img {
     scale: 1.2;
